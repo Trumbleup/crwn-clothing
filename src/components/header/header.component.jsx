334bae72d85bcdecc39562bb553e883ea491fe7c
Danny <Dtrumble12@gmail.com>
2021-07-22 21:25:32 -0500

    Changed the standard / path to / = crwn-clothing in the header icon and the base redirect, only temporary

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -15,7 +15,7 @@ import './header.styles.scss'
 
 const Header = ({ currentUser, hidden }) => (
     <div className='header'>
-        <Link className='logo-container' to ="/">
+        <Link className='logo-container' to ="/crwn-clothing">
             <img src={Logo} className='logo' alt='logo'/>
         </Link>
         <div className='options'>
