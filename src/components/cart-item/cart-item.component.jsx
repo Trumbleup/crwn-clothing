fda8b583d187b096d9c635b338b59cc4a147d3e2
Danny <Dtrumble12@gmail.com>
2021-08-09 20:31:11 -0500

    Changed to CSS in JS styling for cart-dropdown, cart-icon, cart-item, checkout-item

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 
 import './cart-item.styles.scss';
 
+import { CartItemContainer, CartItemImage, ItemDetails } from './cart-item.styles';
+
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-    <div className='cart-item'>
-        <img src={imageUrl} alt='item'/>
-        <div className='item-details'>
+    <CartItemContainer>
+        <CartItemImage src={imageUrl} alt='item'/>
+        <ItemDetails className='item-details'>
             <span className='name'>{name}</span>
             <span className='price'>
                 {quantity} x ${price}
             </span>
-        </div>
-    </div>
+        </ItemDetails>
+    </CartItemContainer>
 )
 
 export default CartItem;
